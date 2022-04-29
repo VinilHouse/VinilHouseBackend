c9f04efe6143731512930ec09d1300fbd202a3a3
강윤식 <re12io@naver.com>
2022-04-29 23:29:48 +0900

    test: add MemberService's methods and test codes

--- src/main/java/com/ssafy/happyhouse5/service/MemberService.java
+++ src/main/java/com/ssafy/happyhouse5/service/MemberService.java
@@ -1,5 +1,20 @@
 package com.ssafy.happyhouse5.service;
 
+import com.ssafy.happyhouse5.dto.member.Member;
+
 public interface MemberService {
 
+    boolean checkExists(String id);
+
+    void register(Member member);
+
+    boolean login(String id, String pass);
+
+    void update(Member member);
+
+    void delete(Member member);
+
+    Member findMemberById(String id);
+
+    Member findMemberByEmail(String email);
 }
