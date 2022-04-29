cf47e942c52e547d699c9281da68e8f4f869ae10
강윤식 <re12io@naver.com>
2022-04-29 23:09:23 +0900

    feat: `Member` Entity for database mapping

--- src/main/java/com/ssafy/happyhouse5/dto/member/Member.java
+++ src/main/java/com/ssafy/happyhouse5/dto/member/Member.java
@@ -1,5 +1,23 @@
 package com.ssafy.happyhouse5.dto.member;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Member {
 
+    @Setter(AccessLevel.NONE)
+    private String id;
+    private String password;
+    private String email;
 }
