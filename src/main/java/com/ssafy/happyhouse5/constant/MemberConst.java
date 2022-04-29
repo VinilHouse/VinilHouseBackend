d75ac16db60eb37f198d6a4d83c758a03cb4d05f
강윤식 <re12io@naver.com>
2022-04-30 00:35:46 +0900

    feat: MemberRestController code. (only login, register api) and validation/session

---
+++ src/main/java/com/ssafy/happyhouse5/constant/MemberConst.java
@@ -0,0 +1,8 @@
+package com.ssafy.happyhouse5.constant;
+
+public interface MemberConst {
+
+    String MEMBER_SESSION = "member";
+    String MEMBER_LOGIN_FAIL_MSG = "fail login. reconfirm args";
+    String MEMBER_BINDING_ERROR = "invalid params. reconfirm args";
+}
--- src/main/java/com/ssafy/happyhouse5/controller/restcontroller/MemberRestController.java
+++ src/main/java/com/ssafy/happyhouse5/controller/restcontroller/MemberRestController.java
@@ -1,5 +1,72 @@
 package com.ssafy.happyhouse5.controller.restcontroller;
 
+import static com.ssafy.happyhouse5.constant.MemberConst.*;
+
+import com.ssafy.happyhouse5.dto.member.Member;
+import com.ssafy.happyhouse5.dto.member.MemberLoginDto;
+import com.ssafy.happyhouse5.dto.member.MemberRegisterDto;
+import com.ssafy.happyhouse5.dto.member.MemberSession;
+import com.ssafy.happyhouse5.service.MemberService;
+import java.net.URI;
+import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping("/api/members")
+@RestController
+@RequiredArgsConstructor
 public class MemberRestController {
 
+    private final MemberService memberService;
+
+    @PostMapping
+    public ResponseEntity<?> register(
+        @Validated @RequestBody MemberRegisterDto memberRegisterDto,
+        BindingResult bindingResult) {
+
+        checkHasBindingError(bindingResult);
+
+        memberService.register(Member.builder()
+            .id(memberRegisterDto.getId())
+            .password(memberRegisterDto.getPassword())
+            .email(memberRegisterDto.getEmail())
+            .build());
+
+        return ResponseEntity.created(URI.create("/api/members/" + memberRegisterDto.getId()))
+            .build();
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(
+        @RequestBody @Validated MemberLoginDto memberLoginDto,
+        BindingResult bindingResult,
+        HttpServletRequest request) {
+
+        checkHasBindingError(bindingResult);
+
+        if (memberService.login(memberLoginDto.getId(), memberLoginDto.getPassword())) {
+            Member member = memberService.findMemberById(memberLoginDto.getId());
+            request.getSession().setAttribute(MEMBER_SESSION, MemberSession.builder()
+                .id(member.getId())
+                .email(member.getEmail())
+                .build());
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(MEMBER_LOGIN_FAIL_MSG);
+    }
+
+    private void checkHasBindingError(BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            throw new RuntimeException(MEMBER_BINDING_ERROR);
+        }
+    }
 }
