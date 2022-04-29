0acef8c37c894b10a0cedbd07746c548b486888d
강윤식 <re12io@naver.com>
2022-04-30 01:03:32 +0900

    feat: code for updating Member in MemberRestController

--- src/main/java/com/ssafy/happyhouse5/constant/MemberConst.java
+++ src/main/java/com/ssafy/happyhouse5/constant/MemberConst.java
@@ -4,5 +4,7 @@ public interface MemberConst {
 
     String MEMBER_SESSION = "member";
     String MEMBER_LOGIN_FAIL_MSG = "fail login. reconfirm args";
+    String MEMBER_LOGOUT_FAIL_MSG = "fail logout. invalid status error";
     String MEMBER_BINDING_ERROR = "invalid params. reconfirm args";
+    String MEMBER_REQUIRED_LOGIN = "required login first";
 }
--- src/main/java/com/ssafy/happyhouse5/controller/restcontroller/MemberRestController.java
+++ src/main/java/com/ssafy/happyhouse5/controller/restcontroller/MemberRestController.java
@@ -1,24 +1,27 @@
 package com.ssafy.happyhouse5.controller.restcontroller;
 
 import static com.ssafy.happyhouse5.constant.MemberConst.*;
+import static org.springframework.http.HttpStatus.*;
 
 import com.ssafy.happyhouse5.dto.member.Member;
 import com.ssafy.happyhouse5.dto.member.MemberLoginDto;
 import com.ssafy.happyhouse5.dto.member.MemberRegisterDto;
 import com.ssafy.happyhouse5.dto.member.MemberSession;
+import com.ssafy.happyhouse5.dto.member.MemberUpdateDto;
 import com.ssafy.happyhouse5.service.MemberService;
 import java.net.URI;
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Slf4j
 @RequestMapping("/api/members")
@@ -49,19 +52,45 @@ public class MemberRestController {
     public ResponseEntity<?> login(
         @RequestBody @Validated MemberLoginDto memberLoginDto,
         BindingResult bindingResult,
-        HttpServletRequest request) {
+        HttpSession session) {
 
         checkHasBindingError(bindingResult);
 
         if (memberService.login(memberLoginDto.getId(), memberLoginDto.getPassword())) {
             Member member = memberService.findMemberById(memberLoginDto.getId());
-            request.getSession().setAttribute(MEMBER_SESSION, MemberSession.builder()
+            session.setAttribute(MEMBER_SESSION, MemberSession.builder()
                 .id(member.getId())
                 .email(member.getEmail())
                 .build());
             return ResponseEntity.ok().build();
         }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(MEMBER_LOGIN_FAIL_MSG);
+        return ResponseEntity.status(UNAUTHORIZED).body(MEMBER_LOGIN_FAIL_MSG);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpSession session) {
+        if (session.isNew() || session.getAttribute(MEMBER_SESSION) == null) {
+            return ResponseEntity.status(UNAUTHORIZED).body(MEMBER_LOGOUT_FAIL_MSG);
+        }
+        session.invalidate();
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<?> update(
+        @SessionAttribute Member member,
+        @RequestBody MemberUpdateDto memberUpdateDto) {
+        if (member == null) {
+            return ResponseEntity.status(UNAUTHORIZED).body(MEMBER_REQUIRED_LOGIN);
+        }
+
+        memberService.update(
+            Member.builder()
+                .id(member.getId())
+                .password(memberUpdateDto.getPassword())
+                .email(memberUpdateDto.getEmail())
+                .build());
+        return ResponseEntity.accepted().build();
     }
 
     private void checkHasBindingError(BindingResult bindingResult) {
