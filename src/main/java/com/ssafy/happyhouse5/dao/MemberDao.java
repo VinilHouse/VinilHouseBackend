78ca095a5a11d5a2847b525adb0e43ccd2a081bf
강윤식 <re12io@naver.com>
2022-04-29 22:39:06 +0900

    code: MemberDao interface's default crud methods

--- src/main/java/com/ssafy/happyhouse5/dao/MemberDao.java
+++ src/main/java/com/ssafy/happyhouse5/dao/MemberDao.java
@@ -1,5 +1,16 @@
 package com.ssafy.happyhouse5.dao;
 
+import com.ssafy.happyhouse5.dto.member.Member;
+
 public interface MemberDao {
 
+    void register(Member member);
+
+    void update(Member member);
+
+    Member findById(String id);
+
+    Member findByEmail(String email);
+
+    void delete(Member member);
 }
