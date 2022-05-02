07982fe5a78e040b7cd0a6fc827e589db1ea47b6
minje0204 <minje0204>
2022-05-03 00:47:05 +0900

    feat: ResponseEntity로 return값 변경 & url mapping /api 추가

--- src/main/java/com/ssafy/happyhouse5/controller/restcontroller/HouseRestController.java
+++ src/main/java/com/ssafy/happyhouse5/controller/restcontroller/HouseRestController.java
@@ -1,6 +1,7 @@
 package com.ssafy.happyhouse5.controller.restcontroller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,7 +12,7 @@ import com.ssafy.happyhouse5.service.HouseService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/house")
+@RequestMapping("/api/house")
 public class HouseRestController {
 
     private HouseService houseService;
@@ -22,12 +23,20 @@ public class HouseRestController {
     }
 
     @GetMapping("/infos")
-    public List<HouseInfo> getHouseInfos(@RequestParam String dongCode) {
-        return houseService.findHouseInfoByDongCode(dongCode);
+    public ResponseEntity<List<HouseInfo>> getHouseInfos(@RequestParam String dongCode) {
+        List<HouseInfo> houseInfoList = houseService.findHouseInfoByDongCode(dongCode);
+        if (houseInfoList == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(houseInfoList);
     }
 
     @GetMapping("/deals")
-    public List<HouseDeal> getHouseDeals(@RequestParam int aptCode) {
-        return houseService.findHouseDealListByAptCode(aptCode);
+    public ResponseEntity<List<HouseDeal>> getHouseDeals(@RequestParam int aptCode) {
+        List<HouseDeal> houseDealList = houseService.findHouseDealListByAptCode(aptCode);
+        if (houseDealList == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(houseDealList);
     }
 }
