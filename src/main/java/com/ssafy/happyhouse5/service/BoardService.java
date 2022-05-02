9c40aa5d63c63165b5a30e0d55408844e3432f0a
강윤식 <re12io@naver.com>
2022-05-02 15:26:25 +0900

    test: BoardService interface and Test codes

--- src/main/java/com/ssafy/happyhouse5/service/BoardService.java
+++ src/main/java/com/ssafy/happyhouse5/service/BoardService.java
@@ -1,5 +1,19 @@
 package com.ssafy.happyhouse5.service;
 
+import com.ssafy.happyhouse5.dto.board.Board;
+import com.ssafy.happyhouse5.dto.member.Member;
+import com.ssafy.happyhouse5.service.impl.BoardSearchOption;
+import java.util.List;
+
 public interface BoardService {
 
+    void create(Board board);
+
+    void update(Member memberId, Board board);
+
+    Board selectById(int id);
+
+    List<Board> findAll();
+
+    List<Board> findByOption(BoardSearchOption boardSearchOption, String query);
 }
