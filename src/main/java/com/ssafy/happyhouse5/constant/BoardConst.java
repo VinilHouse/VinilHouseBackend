5d5dbc16fdc88de929d40d09f51ecc381085ed99
강윤식 <re12io@naver.com>
2022-05-02 15:39:24 +0900

    feat: write BoardServiceImpl and BoardConst for msg

---
+++ src/main/java/com/ssafy/happyhouse5/constant/BoardConst.java
@@ -0,0 +1,6 @@
+package com.ssafy.happyhouse5.constant;
+
+public interface BoardConst {
+    String WRITER_ONLY_MODIFY = "only writer modify board.";
+    String BOARD_NOT_FOUND = "not exists board. (by boardId)";
+}
