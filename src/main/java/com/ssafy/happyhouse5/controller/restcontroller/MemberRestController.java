0e37afc3830f629d12c839f976641f2b5e5ea1de
강윤식 <re12io@naver.com>
2022-05-02 21:57:27 +0900

    feat: add cors originPatterns

--- src/main/java/com/ssafy/happyhouse5/controller/restcontroller/MemberRestController.java
+++ src/main/java/com/ssafy/happyhouse5/controller/restcontroller/MemberRestController.java
@@ -16,6 +16,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -27,6 +28,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RequestMapping("/api/members")
+@CrossOrigin(originPatterns = {
+    "http://127.0.0.1:5500/",
+    "http://localhost:5500/"
+}, allowCredentials = "true")
 @RestController
 @RequiredArgsConstructor
 public class MemberRestController {
