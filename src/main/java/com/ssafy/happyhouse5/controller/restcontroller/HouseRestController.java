2a8656b7b6be5398cc376dc3d718477d4ba96bdc
강윤식 <re12io@naver.com>
2022-05-08 15:23:40 +0900

    feat: convert controller for jpa fit service (Long ID etc...) and member(id) -> member(Ident)

--- src/main/java/com/ssafy/happyhouse5/controller/restcontroller/HouseRestController.java
+++ src/main/java/com/ssafy/happyhouse5/controller/restcontroller/HouseRestController.java
@@ -1,24 +1,21 @@
 package com.ssafy.happyhouse5.controller.restcontroller;
 
+import com.ssafy.happyhouse5.dto.house.HouseDealResponseDto;
+import com.ssafy.happyhouse5.dto.house.HouseInfoResponseDto;
+import com.ssafy.happyhouse5.entity.HouseDeal;
+import com.ssafy.happyhouse5.entity.HouseInfo;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import com.ssafy.happyhouse5.dto.house.HouseDeal;
-import com.ssafy.happyhouse5.dto.house.HouseInfo;
 import com.ssafy.happyhouse5.service.HouseService;
-import java.util.List;
 
 @RequestMapping("/api/houses")
-@CrossOrigin(originPatterns = {
-    "http://127.0.0.1:5500/",
-    "http://localhost:5500/"
-}, allowCredentials = "true")
 @RestController
 @RequiredArgsConstructor
 public class HouseRestController {
@@ -26,20 +23,24 @@ public class HouseRestController {
     private final HouseService houseService;
 
     @GetMapping("/info")
-    public ResponseEntity<List<HouseInfo>> getHouseInfos(@RequestParam String dongCode) {
-        List<HouseInfo> houseInfoList = houseService.findHouseInfoByDongCode(dongCode);
-        if (houseInfoList == null) {
+    public ResponseEntity<List<HouseInfoResponseDto>> getHouseInfos(@RequestParam String dongName) {
+        List<HouseInfo> houseInfoList = houseService.findHouseInfoByDongName(dongName);
+        if (houseInfoList.size() == 0) {
             return ResponseEntity.noContent().build();
         }
-        return ResponseEntity.ok(houseInfoList);
+        return ResponseEntity.ok(houseInfoList.stream()
+            .map(HouseInfoResponseDto::new)
+            .collect(Collectors.toList()));
     }
 
     @GetMapping("/deal")
-    public ResponseEntity<List<HouseDeal>> getHouseDeals(@RequestParam int aptCode) {
+    public ResponseEntity<List<HouseDealResponseDto>> getHouseDeals(@RequestParam Long aptCode) {
         List<HouseDeal> houseDealList = houseService.findHouseDealListByAptCode(aptCode);
         if (houseDealList == null) {
             return ResponseEntity.noContent().build();
         }
-        return ResponseEntity.ok(houseDealList);
+        return ResponseEntity.ok(houseDealList.stream()
+            .map(HouseDealResponseDto::new)
+            .collect(Collectors.toList()));
     }
 }
