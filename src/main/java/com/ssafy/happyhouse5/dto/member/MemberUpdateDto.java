12df9b4fcfb7c926dff2d9c1b030d61f28cc9859
강윤식 <re12io@naver.com>
2022-05-08 15:30:58 +0900

    fix: add setter for test code

--- src/main/java/com/ssafy/happyhouse5/dto/member/MemberUpdateDto.java
+++ src/main/java/com/ssafy/happyhouse5/dto/member/MemberUpdateDto.java
@@ -2,9 +2,11 @@ package com.ssafy.happyhouse5.dto.member;
 
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.lang.Nullable;
 
 @Getter
+@Setter
 public class MemberUpdateDto {
     @Nullable
     private String password;
