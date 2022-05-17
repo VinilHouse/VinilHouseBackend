e3137d483343ec9ff6341f27dea9246560bb2cbb
kangyunsik <re12io@ajou.ac.kr>
2022-05-17 14:59:11 +0900

    feat: search by prefix 구현.

--- src/main/java/com/ssafy/happyhouse5/repository/HouseInfoRepository.java
+++ src/main/java/com/ssafy/happyhouse5/repository/HouseInfoRepository.java
@@ -2,11 +2,17 @@ package com.ssafy.happyhouse5.repository;
 
 import com.ssafy.happyhouse5.entity.HouseInfo;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface HouseInfoRepository extends JpaRepository<HouseInfo, Long> {
 
     @Query("select i from HouseInfo i join fetch i.baseAddress b where b.dongName = :dongName")
-    List<HouseInfo> findHouseInfoBydongName(String dongName);
+    List<HouseInfo> findHouseInfoBydongName(@Param("dongName") String dongName);
+
+    @Query("select i from HouseInfo i where i.aptName like :prefix%")
+    Page<HouseInfo> findHouseInfoByAptPrefix(@Param("prefix")String prefix, Pageable pageable);
 }
--- src/main/java/com/ssafy/happyhouse5/service/HouseService.java
+++ src/main/java/com/ssafy/happyhouse5/service/HouseService.java
@@ -4,10 +4,14 @@ package com.ssafy.happyhouse5.service;
 import com.ssafy.happyhouse5.entity.HouseDeal;
 import com.ssafy.happyhouse5.entity.HouseInfo;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface HouseService {
 
     List<HouseInfo> findHouseInfoByDongName(String dongName);
 
     List<HouseDeal> findHouseDealListByAptCode(Long aptCode);
+
+    Page<HouseInfo> findHouseInfoByPrefix(String prefix, Pageable pageable);
 }
