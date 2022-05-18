832621fbe1aac327ee47feaf3002c579b781ef4f
kangyunsik <re12io@ajou.ac.kr>
2022-05-18 18:16:04 +0900

    feat: 핸들러 매칭, 인수 매칭, 메서드 매칭 익셉션 추가

--- src/main/java/com/ssafy/happyhouse5/constant/CommonConst.java
+++ src/main/java/com/ssafy/happyhouse5/constant/CommonConst.java
@@ -3,5 +3,5 @@ package com.ssafy.happyhouse5.constant;
 public interface CommonConst {
 
     String BAD_REQUEST_MSG = "Bad params or request body.";
-
+    String NO_HANDLER_MSG = "No handler matches.";
 }
