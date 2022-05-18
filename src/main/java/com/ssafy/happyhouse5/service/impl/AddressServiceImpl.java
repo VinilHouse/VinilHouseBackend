7c1e642500fbfa7ef6cafdc179621bfdb12e9698
kangyunsik <re12io@ajou.ac.kr>
2022-05-18 17:54:57 +0900

    feat: Transactional(readOnly = true) 추가

--- src/main/java/com/ssafy/happyhouse5/service/impl/AddressServiceImpl.java
+++ src/main/java/com/ssafy/happyhouse5/service/impl/AddressServiceImpl.java
@@ -6,9 +6,11 @@ import com.ssafy.happyhouse5.service.AddressService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class AddressServiceImpl implements AddressService {
 
     private final BaseAddressRepository baseAddressRepository;
