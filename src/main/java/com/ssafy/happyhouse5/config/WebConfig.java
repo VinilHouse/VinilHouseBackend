538e8f1095fac3875d8b41c81e0b79be988b86c7
kangyunsik <re12io@ajou.ac.kr>
2022-05-20 10:40:43 +0900

    chore: cors 클라이언트 origin 추가

--- src/main/java/com/ssafy/happyhouse5/config/WebConfig.java
+++ src/main/java/com/ssafy/happyhouse5/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry
             .addMapping("/**")
             .allowedOrigins("http://127.0.0.1:3000/", "http://localhost:3000/",
-                "http://127.0.0.1:5500/", "http://localhost:5500/")
+                "https://vinil-house-client-clone.vercel.app/")
             .allowedMethods("GET", "POST", "PATCH", "DELETE")
             .allowCredentials(true)
             .exposedHeaders("Set-Cookie");
