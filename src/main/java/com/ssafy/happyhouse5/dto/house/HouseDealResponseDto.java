bd8a8466d0829cd61bf5c21b1e481ec2020e92cb
kangyunsik <re12io@ajou.ac.kr>
2022-05-21 20:42:35 +0900

    feat: HouseInfo 이미지 Setter 추가 및 HouseDeal 에 대한 조회 시 응답 데이터에 HouseInfo 추가된 Dto 로 변경.

--- src/main/java/com/ssafy/happyhouse5/dto/house/HouseDealResponseDto.java
+++ src/main/java/com/ssafy/happyhouse5/dto/house/HouseDealResponseDto.java
@@ -1,34 +1,13 @@
 package com.ssafy.happyhouse5.dto.house;
 
-import com.ssafy.happyhouse5.entity.HouseDeal;
-import lombok.Getter;
-import lombok.Setter;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
+@AllArgsConstructor
 public class HouseDealResponseDto {
 
-    private Long id;
-    private Long aptCode;
-    private Double dealAmount;
-    private int dealYear;
-    private int dealMonth;
-    private int dealDay;
-    private String area;
-    private String floor;
-    private String type;
-    private String rentMoney;
-
-    public HouseDealResponseDto(HouseDeal houseDeal) {
-        this.id = houseDeal.getId();
-        this.aptCode = houseDeal.getHouseInfo().getAptCode();
-        this.dealAmount = houseDeal.getDealAmount();
-        this.dealYear = houseDeal.getDealYear();
-        this.dealMonth = houseDeal.getDealMonth();
-        this.dealDay = houseDeal.getDealDay();
-        this.area = houseDeal.getArea();
-        this.floor = houseDeal.getFloor();
-        this.type = houseDeal.getType();
-        this.rentMoney = houseDeal.getRentMoney();
-    }
+    HouseInfoResponseDto houseInfoResponseDto;
+    List<HouseDealItemDto> houseDealItemDtoList;
 }
