051a2a6ea515e251ae1311236db0d9469a3c4f01
kangyunsik <re12io@ajou.ac.kr>
2022-05-23 20:42:29 +0900

    test: memberService 의 아파트 댓글 기본 등록/수정/삭제/예외 케이스 추가

--- src/test/java/com/ssafy/happyhouse5/service/MemberServiceTest.java
+++ src/test/java/com/ssafy/happyhouse5/service/MemberServiceTest.java
@@ -3,13 +3,18 @@ package com.ssafy.happyhouse5.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.ssafy.happyhouse5.dto.comment.CommentRegistDto;
+import com.ssafy.happyhouse5.dto.comment.CommentUpdateDto;
 import com.ssafy.happyhouse5.dto.member.MemberUpdateDto;
+import com.ssafy.happyhouse5.entity.Comment;
 import com.ssafy.happyhouse5.entity.Favorite;
 import com.ssafy.happyhouse5.entity.HouseInfo;
 import com.ssafy.happyhouse5.entity.Member;
+import com.ssafy.happyhouse5.exception.comment.CommentNotFoundException;
 import com.ssafy.happyhouse5.exception.favorite.FavoriteDuplicateException;
 import com.ssafy.happyhouse5.exception.favorite.FavoriteNotFoundException;
 import com.ssafy.happyhouse5.exception.house.HouseInfoNotFoundException;
+import com.ssafy.happyhouse5.exception.member.MemberAuthException;
 import com.ssafy.happyhouse5.exception.member.MemberNotFoundException;
 import java.util.List;
 import javax.persistence.EntityManager;
@@ -210,5 +215,105 @@ class MemberServiceTest {
 
         List<HouseInfo> favoriteHouseInfos = memberService.getFavoriteHouseInfo(member.getId());
         assertThat(favoriteHouseInfos).contains(houseInfo1, houseInfo2);
+    }
+
+    @Test
+    @DisplayName("아파트 댓글 등록 테스트")
+    void createCommentTest() {
+        Member member = memberService.findMemberByIdent(MEMBER_ID);
+        HouseInfo houseInfo = new HouseInfo("apt1");
+
+        em.persist(member);
+        em.persist(houseInfo);
+
+        CommentRegistDto commentRegistDto = new CommentRegistDto();
+        commentRegistDto.setTitle("comment title");
+        commentRegistDto.setContent("comment content");
+
+        Long commentId = memberService.createComment(member.getId(), houseInfo.getAptCode()
+            , commentRegistDto);
+
+        Comment findComment = em.find(Comment.class, commentId);
+
+        assertThat(findComment.getTitle()).isEqualTo(commentRegistDto.getTitle());
+        assertThat(findComment.getContent()).isEqualTo(commentRegistDto.getContent());
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 회원의 아파트 댓글 등록 테스트")
+    void createCommentTestNotExistMember() {
+        HouseInfo houseInfo = new HouseInfo("apt1");
+
+        em.persist(houseInfo);
+
+        CommentRegistDto commentRegistDto = new CommentRegistDto();
+        commentRegistDto.setTitle("comment title");
+        commentRegistDto.setContent("comment content");
+
+        assertThrows(MemberNotFoundException.class, () ->
+            memberService.createComment(999L, houseInfo.getAptCode(), commentRegistDto));
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 아파트에 대한 댓글 등록 테스트")
+    void createCommentTestNotExistApt() {
+        Member member = memberService.findMemberByIdent(MEMBER_ID);
+        HouseInfo houseInfo = new HouseInfo("apt1");
+
+        em.persist(member);
+        em.persist(houseInfo);
+
+        CommentRegistDto commentRegistDto = new CommentRegistDto();
+        commentRegistDto.setTitle("new comment title");
+        commentRegistDto.setContent("new comment content");
+
+        assertThrows(HouseInfoNotFoundException.class,
+            () -> memberService.createComment(member.getId(), 9876543210L, commentRegistDto));
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 아파트 댓글에 대한 수정/삭제 테스트")
+    void updateCommentNotExistCommentCaseTest() {
+        Member member = memberService.findMemberByIdent(MEMBER_ID);
+
+        em.persist(member);
+
+        CommentUpdateDto commentUpdateDto = new CommentUpdateDto();
+        commentUpdateDto.setTitle("comment title");
+        commentUpdateDto.setContent("comment content");
+
+        assertThrows(CommentNotFoundException.class,
+            () -> memberService.updateComment(member.getId(), 9876543210L, commentUpdateDto));
+
+        assertThrows(CommentNotFoundException.class,
+            () -> memberService.deleteComment(member.getId(), 9876543210L));
+    }
+
+    @Test
+    @DisplayName("정상적인 아파트 댓글을 수정하는 테스트")
+    void updateCommentTest() {
+        Member member = memberService.findMemberByIdent(MEMBER_ID);
+        HouseInfo houseInfo = new HouseInfo("apt1");
+
+        Comment comment = new Comment();
+        comment.setMember(member);
+        comment.setHouseInfo(houseInfo);
+
+        em.persist(member);
+        em.persist(houseInfo);
+        em.persist(comment);
+
+        CommentUpdateDto commentUpdateDto = new CommentUpdateDto();
+        commentUpdateDto.setTitle("new Title");
+        commentUpdateDto.setContent("new Content");
+
+        memberService.updateComment(member.getId(), comment.getId(), commentUpdateDto);
+
+        Comment updatedComment = em.find(Comment.class, comment.getId());
+
+        assertThat(updatedComment).isNotNull();
+
+        assertThat(updatedComment.getTitle()).isEqualTo(commentUpdateDto.getTitle());
+        assertThat(updatedComment.getContent()).isEqualTo(commentUpdateDto.getContent());
     }
 }
