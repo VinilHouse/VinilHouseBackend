5b5cda91e81a5da1c493e55ea6c2ed73ae390bc0
kangyunsik <re12io@ajou.ac.kr>
2022-05-23 21:34:08 +0900

    feat: locale, timezone 설정 추가

--- src/main/java/com/ssafy/happyhouse5/config/WebConfig.java
+++ src/main/java/com/ssafy/happyhouse5/config/WebConfig.java
@@ -1,5 +1,8 @@
 package com.ssafy.happyhouse5.config;
 
+import java.util.Locale;
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,5 +19,11 @@ public class WebConfig implements WebMvcConfigurer {
             .allowedMethods("GET", "POST", "PATCH", "DELETE")
             .allowCredentials(true)
             .exposedHeaders("Set-Cookie");
+    }
+
+    @PostConstruct
+    public void timeZoneAndLocaleSet() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+        Locale.setDefault(Locale.KOREA);
     }
 }
