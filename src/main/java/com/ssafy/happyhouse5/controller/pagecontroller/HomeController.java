24b7010cef3973a62d0ae58fbeb111daadc411f8
kangyunsik <re12io@ajou.ac.kr>
2022-05-23 22:04:37 +0900

    feat: redirect to swagger page 추가

--- src/main/java/com/ssafy/happyhouse5/controller/pagecontroller/HomeController.java
+++ src/main/java/com/ssafy/happyhouse5/controller/pagecontroller/HomeController.java
@@ -1,5 +1,13 @@
 package com.ssafy.happyhouse5.controller.pagecontroller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
 
+    @GetMapping(value = {"/", "/index"})
+    public String redirectToSwagger() {
+        return "redirect:/swagger-ui/";
+    }
 }
