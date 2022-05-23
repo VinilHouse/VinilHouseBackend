5e70a18fedf35bda4b247df1fd27ae239915868b
kangyunsik <re12io@ajou.ac.kr>
2022-05-23 16:17:22 +0900

    feat: 아파트 정보에 대해 사용자의 의견 엔티티 + 레포지토리 생성.

---
+++ src/main/java/com/ssafy/happyhouse5/entity/Comment.java
@@ -0,0 +1,36 @@
+package com.ssafy.happyhouse5.entity;
+
+import static javax.persistence.FetchType.LAZY;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "comment")
+@NoArgsConstructor
+public class Comment {
+
+    @Id @GeneratedValue
+    @Column(name = "comment_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "apt_code")
+    private HouseInfo houseInfo;
+
+    private String title;
+
+    private String content;
+}
