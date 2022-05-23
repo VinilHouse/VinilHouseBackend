6f44e78616c69f79be68bcf06da0feb8d90d73df
kangyunsik <re12io@ajou.ac.kr>
2022-05-23 16:57:32 +0900

    feat: comment regist, update dto 생성.

---
+++ src/main/java/com/ssafy/happyhouse5/dto/comment/CommentRegistDto.java
@@ -0,0 +1,10 @@
+package com.ssafy.happyhouse5.dto.comment;
+
+import lombok.Data;
+
+@Data
+public class CommentRegistDto {
+
+    private String title;
+    private String content;
+}
---
+++ src/main/java/com/ssafy/happyhouse5/dto/comment/CommentUpdateDto.java
@@ -0,0 +1,10 @@
+package com.ssafy.happyhouse5.dto.comment;
+
+import lombok.Data;
+
+@Data
+public class CommentUpdateDto {
+
+    private String title;
+    private String content;
+}
