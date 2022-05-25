6ebd39ffedb97be2ae4a9c6921c6109b206438f0
kangyunsik <re12io@ajou.ac.kr>
2022-05-25 22:54:08 +0900

    feat: address converter (외부 api <rest-template> 로 주소 위도 경도 가져오도록 틀만 작성.

---
+++ src/main/java/com/ssafy/happyhouse5/util/LatLng.java
@@ -0,0 +1,9 @@
+package com.ssafy.happyhouse5.util;
+
+import lombok.Data;
+
+@Data
+public class LatLng {
+    private Double lat;
+    private Double lng;
+}
---
+++ src/main/java/com/ssafy/happyhouse5/util/impl/AddressConverterImpl.java
@@ -0,0 +1,12 @@
+package com.ssafy.happyhouse5.util.impl;
+
+import com.ssafy.happyhouse5.util.AddressConverter;
+import com.ssafy.happyhouse5.util.LatLng;
+
+public class AddressConverterImpl implements AddressConverter {
+
+    @Override
+    public LatLng convert(String address) {
+        return null;
+    }
+}
